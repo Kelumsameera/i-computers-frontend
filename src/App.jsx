9c5f4db6afe0328cc1b7a3becda99e32a3799640
Kelumsameera <sameerakelum16@gmail.com>
2025-10-19 00:57:15 +0530

    day 14

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
 import AdminPage from "./pages/adminPage";
 import TestPage from "./pages/testPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-right" />
       <div className="w-full h-screen">
         <Routes>
           <Route path="/*" element={<HomePage />} />
