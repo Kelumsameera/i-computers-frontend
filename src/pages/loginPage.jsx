dc76b215c2eb1e59e57354d4d0584aeddeffcf42
Kelumsameera <sameerakelum16@gmail.com>
2025-10-30 12:39:32 +0530

    ✍️add map functionality with axios  with useEfect hooks

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,11 +1,108 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 
-function loginPage() {
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function login() {
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_BACKEND_URL + "/users/login",
+        {
+          email: email,
+          password: password,
+        }
+      );
+
+      console.log(res.data);
+      const data = res.data;
+      if (res.data.role == "admin") {
+        // window.location.href = "/admin";
+        navigate("/admin");
+      } else {
+        // window.location.href = "/";
+        navigate("/");
+      }
+
+      console.log("Login success:", data);
+      toast.success("Login successful! Welcome back. ");
+    } catch (err) {
+      console.error("Error during login:", err);
+      toast.error("Something went wrong. Please try again.");
+    }
+  }
+
   return (
-    <div className=" w-full h-screen bg-[url('/bg.jpg')]">
-      <h1>loginPage</h1>
+    <div className="w-full h-screen bg-[url('/bg.jpg')] bg-center bg-cover bg-no-repeat flex">
+      {/* Left section */}
+      <div className="w-[50%] h-full flex justify-center items-center flex-col p-[50px]">
+        <img
+          src="/logo.png"
+          alt="logo"
+          className="w-[200px] h-[200px] mb-[20px] object-cover"
+        />
+        <h1 className="text-[50px] text-gold text-shadow-accent text-center font-bold">
+          Plug In. Power Up. Play Hard.
+        </h1>
+        <p className="text-[30px] text-white italic text-center">
+          Your Ultimate Destination for Gaming Gear
+        </p>
+      </div>
+
+      {/* Right section */}
+      <div className="w-[50%] h-full flex justify-center items-center">
+        <div className="w-[450px] h-[500px] backdrop-blur-lg shadow-2xl rounded-2xl flex flex-col justify-center items-center p-[20px]">
+          <h1 className="text-[40px] font-bold mb-[20px] text-white text-shadow-accent">
+            Login
+          </h1>
+
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="your email"
+            className="w-[350px] h-[50px] mb-[20px] rounded-lg border border-accent p-[10px] text-[20px] focus:outline-none focus:ring-2 focus:ring-gold"
+          />
+
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="your password"
+            className="w-[350px] h-[50px] rounded-lg border border-accent p-[10px] text-[20px] focus:outline-none focus:ring-2 focus:ring-gold"
+          />
+
+          <p className="text-white w-full mb-[20px] p-[10px] text-right not-italic">
+            Forget your password?
+            <Link to="/reset-password" className="text-gold italic">
+              Reset here
+            </Link>
+          </p>
+
+          <button
+            onClick={login}
+            className="w-[350px] h-[50px] mb-[20px] font-bold rounded-lg bg-accent text-white border-accent border-[2px] p-[10px] text-[20px] hover:bg-transparent hover:text-accent transition-all"
+          >
+            Login
+          </button>
+
+          <p className="text-white not-italic">
+            Don't have an account?
+            <Link to="/register" className="text-gold italic">
+              Register here
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default loginPage;
+export default LoginPage;
